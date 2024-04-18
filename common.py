ae59b1aa701e19ef98df80df536abfdee692a729
George Luo <focusdlaw@gmail.com>
2024-04-19 00:13:34 +1000

    refactor: Refactored train loop and break them to a few helper functions. Add proper comments.

--- common.py
+++ common.py
@@ -13,8 +13,8 @@ import torch.nn.functional as F
 ########################################################################################################################
 # NOTE: Set the device based on CUDA availability
 ########################################################################################################################
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Using device: {device}")
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {DEVICE}")
 
 
 
@@ -24,10 +24,26 @@ print(f"Using device: {device}")
 
 # Prototypical network implementation based on https://colab.research.google.com/drive/1TPL2e3v8zcDK00ABqH3R0XXNJtJnLBCd?usp=sharing#scrollTo=UW5Rxifk7Kru
 class Prototypical(nn.Module):
-    #Initialise model with our attention model as base CNN and flattening fc layer
+    """
+    A Prototypical Network model for few-shot learning which uses a CNN as the backbone for feature extraction,
+    and computes prototypes to classify query images based on the minimum Euclidean distance to support set prototypes.
+
+    :param None: Inherits from nn.Module, does not require parameters to be passed explicitly during initialization.
+    """
     def __init__(self):
+        """
+        Initialise model with our attention model as base CNN and flattening fc layer
+        """
         super(Prototypical, self).__init__()
+        ########################################################################################
+        # NOTE: Choose your base CNN to test. Our attention-based model CBAMAttentionMN is the best.
+        ########################################################################################
         self.baseCNN = CBAMAttentionMN((224, 224))
+        # self.baseCNN = models.mobilenet_v2(weights=models.MobileNet_V2_Weights.IMAGENET1K_V1)
+        # self.baseCNN = models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1)
+        # self.baseCNN = models.vgg19(weights=models.VGG19_Weights.IMAGENET1K_V1)
+        # self.baseCNN = models.vit_b_16(weights=models.ViT_B_16_Weights.IMAGENET1K_V1)
+        ########################################################################################
         self.support_images= []
         self.support_labels = []
     
@@ -37,6 +53,14 @@ class Prototypical(nn.Module):
         support_labels: torch.Tensor,
         query_images: torch.Tensor,
     ) -> torch.Tensor:
+        """
+        Forward pass of the model which computes the similarity between query images and class prototypes defined by support images.
+
+        :param support_images: Tensor containing images of the support set.
+        :param support_labels: Tensor containing labels of the support set.
+        :param query_images: Tensor containing images to be classified.
+        :return: Tensor of scores representing the distances of each query image to the class prototypes.
+        """
         support_features = self.baseCNN.forward(support_images)
         query_features = self.baseCNN.forward(query_images)
 
@@ -51,6 +75,13 @@ class Prototypical(nn.Module):
 
 # Attention module code based on https://github.com/EscVM/EscVM_YT/blob/master/Notebooks/0%20-%20TF2.X%20Tutorials/tf_2_visual_attention.ipynb
 class ChannelAttention(nn.Module):
+    """
+    Channel attention module which emphasizes informative features by learning to use global spatial information
+    of the channels in convolutional neural networks.
+
+    :param filters: Number of filters in the previous convolution layer.
+    :param ratio: Reduction ratio for dimensionality reduction in the attention mechanism.
+    """
     def __init__(self, filters, ratio):
         super(ChannelAttention, self).__init__()
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
@@ -63,25 +94,51 @@ class ChannelAttention(nn.Module):
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, inputs):
+        """
+        Forward pass of the channel attention module.
+
+        :param inputs: Input tensor with feature maps from the previous layer.
+        :return: Tensor, where attention has been applied to the input feature maps.
+        """
         avg_out = self.shared_mlp(self.avg_pool(inputs).view(inputs.size(0), -1)).view(inputs.size(0), -1, 1, 1)
         max_out = self.shared_mlp(self.max_pool(inputs).view(inputs.size(0), -1)).view(inputs.size(0), -1, 1, 1)
         attention = self.sigmoid(avg_out + max_out)
         return inputs * attention
 
+
 class SpatialAttention(nn.Module):
+    """
+    Spatial attention module that highlights salient features in spatial dimensions of the input feature maps,
+    improving the focus on relevant parts of the input data.
+
+    :param kernel_size: Size of the convolution kernel used for creating the attention map.
+    """
     def __init__(self, kernel_size=7):
         super(SpatialAttention, self).__init__()
         self.conv2d = nn.Conv2d(2, 1, kernel_size=kernel_size, padding=kernel_size//2, bias=False)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, inputs):
+        """
+        Forward pass of the spatial attention module.
+
+        :param inputs: Input tensor with feature maps from the previous layer.
+        :return: Tensor, where attention has been applied spatially to the input feature maps.
+        """
         avg_out = torch.mean(inputs, dim=1, keepdim=True)
         max_out, _ = torch.max(inputs, dim=1, keepdim=True)
         x = torch.cat([avg_out, max_out], dim=1)
         attention = self.sigmoid(self.conv2d(x))
         return inputs * attention
 
+
 class CBAMAttentionMN(nn.Module):
+    """
+    Convolutional Block Attention Module (CBAM) MobileNet model, an attention-enhanced network using both channel
+    and spatial attention mechanisms to improve feature representation in convolutional neural networks.
+
+    :param input_shape: Expected input shape for the model, used to configure layers.
+    """
     def __init__(self, input_shape):
         super(CBAMAttentionMN, self).__init__()
         # Path to the locally saved weights
@@ -91,7 +148,7 @@ class CBAMAttentionMN(nn.Module):
         model = models.mobilenet_v2(weights=None)
 
         # Load the weights from a local file instead of using MobileNet_V2_Weights
-        model_weights = torch.load(weights_path, map_location=device)
+        model_weights = torch.load(weights_path, map_location=DEVICE)
         model.load_state_dict(model_weights)
 
         # We are using only the features of MobileNetV2
@@ -112,6 +169,12 @@ class CBAMAttentionMN(nn.Module):
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, inputs):
+        """
+        Forward pass of the CBAM-enhanced MobileNet model.
+
+        :param inputs: Input tensor containing the data to be processed.
+        :return: Output tensor after applying both channel and spatial attention mechanisms.
+        """
         x = self.baseModel(inputs)
         x = self.channel_attention(x)
         x = self.spatial_attention(x)
@@ -167,29 +230,6 @@ def load_single_image(image_file_path: str) -> Image:
     # Convert the image to RGB format
     image = image.convert('RGB')
 
-    # The following are examples on how you might manipulate the image.
-    # See full documentation on Pillow (PIL): https://pillow.readthedocs.io/en/stable/
-
-    # To make the image 50% smaller
-    # Determine image dimensions
-    # width, height = image.size
-    # new_width = int(width * 0.50)
-    # new_height = int(height * 0.50)
-    # image = image.resize((new_width, new_height))
-
-    # To crop the image
-    # (left, upper, right, lower) = (20, 20, 100, 100)
-    # image = image.crop((left, upper, right, lower))
-
-    # To view an image
-    # image.show()
-
-    # Return either the pixels as array - image_array
-    # To convert to a NumPy array
-    # image_array = np.asarray(image)
-    # return image_array
-
-    # or return the image
     return image
 
 
@@ -228,9 +268,6 @@ def save_model(model: Any, target: str, output_dir: str):
     :param target: the target value - can be useful to name the model file for the target it is intended for
     :param output_dir: the output directory to same one or more model files.
     """
-    # TODO: implement your model saving code here
-    # model_dir = os.path.join(output_dir, f'model_{target}')
-    
     # Check if the directory exists, and if not, create it
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -259,9 +296,7 @@ def load_model(trained_model_dir: str, target_column_name: str) -> Any:
     model_path = os.path.join(trained_model_dir, f'model_{target_column_name}.pth')
     print(f"Loaded model from {model_path}")
 
-    # model = torch.load(model_path)
-
     # Load the model with map_location to ensure compatibility across different devices
-    model = torch.load(model_path, map_location=device)
+    model = torch.load(model_path, map_location=DEVICE)
 
     return model
